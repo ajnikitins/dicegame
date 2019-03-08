dafe303a5df629425fa83441ea67920c6f4d8991
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2019-03-08 19:13:43 +0200

    add basic main menu functions

--- src/main/java/com/dicegame/Main.java
+++ src/main/java/com/dicegame/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
 
     Scene scene = new Scene(root);
     scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
--- src/main/java/com/dicegame/controller/FXMLController.java
+++ src/main/java/com/dicegame/controller/FXMLController.java
@@ -3,6 +3,7 @@ package com.dicegame.controller;
 import com.dicegame.enums.ConnectionState;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -23,6 +24,8 @@ public class FXMLController implements Initializable {
   @FXML
   private TextField portField;
 
+  @FXML
+  private TextField roomSizeField;
 
   @FXML
   private Button hostButton;
@@ -31,7 +34,55 @@ public class FXMLController implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    connectionGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
 
+      if (portField.getText().equals("")) {
+        portField.setText("35555");
+      }
+
+      if (connectionState == ConnectionState.HOST) {
+        connectionState = ConnectionState.GUEST;
+        ipField.setDisable(false);
+        roomSizeField.setDisable(true);
+        hostButton.setText("Connect");
+
+      } else {
+        connectionState = ConnectionState.HOST;
+        ipField.setText("localhost");
+        ipField.setDisable(true);
+        roomSizeField.setDisable(false);
+        hostButton.setText("Host");
+      }
+    });
+
+    portField.textProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue.matches("\\d*")) {
+        portField.setText(newValue.replaceAll("\\D+", ""));
+      }
+
+      if (!portField.getText().equals("")) {
+        int num = Integer.parseInt(portField.getText());
+
+        if (num > 65535) {
+          portField.setText("65535");
+        }
+      }
+    });
+
+    roomSizeField.textProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue.matches("\\d*")) {
+        roomSizeField.setText(newValue.replaceAll("\\D+", ""));
+      }
+
+      if (!roomSizeField.getText().equals("")) {
+        int num = Integer.parseInt(roomSizeField.getText());
+
+        if (num > 100) {
+          roomSizeField.setText("100");
+        }
+      }
+
+    });
   }
 
   @FXML
